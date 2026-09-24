76e183b45b0f54e56b1e447eb6d6b1ee6a3c2114
toanthaytrung <npdtrung2009@gmail.com>
2026-09-24 20:09:24 +0700

    Deploy k12_de01/exam_data.js

---
+++ k12_de01/exam_data.js
@@ -0,0 +1,14 @@
+const EXAM_DATA = {
+  "part1": [],
+  "part2": [],
+  "part3": [],
+  "answerKey": {
+    "p1": {},
+    "p2": {},
+    "p3": {}
+  },
+  "title": "ĐỀ THI TỐT NGHIỆP THPT MÔN TOÁN - SỐ 01",
+  "subtitle": "Chương trình GDPT 2018 - Khối 12 - Thời gian: 90 phút",
+  "durationMinutes": 90,
+  "unlockSolutionScore": 6.5
+};
